e99c7a37ab49e388ef11aeef456c3eaf929748d7
thomas murat <thomas.murat974@gmail.com>
2023-07-24 18:42:59 +0200

    point 5 partiel

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,26 @@
 class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new # Needed to instantiate the form_with
+  end
+
+  def create
+    @task = Task.new(params[:task])
+    @task.save # Will raise ActiveModel::ForbiddenAttributesError
+    redirect_to task_params(@task)
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
